9d44a066166ecfe981c337a63240e2fa68fe63f4
Yago Russo <yagrrusso@gmail.com>
2024-08-27 00:07:28 -0300

    feat: main shadnc components

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,11 @@ import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { langs } from '../i18n'
 import { useTranslation } from 'react-i18next'
+import { Toaster } from '@/components/ui/toaster'
+import { useToast } from '@/components/ui/use-toast'
+import { ThemeProvider } from '@/contexts/theme'
+import { Button } from '@/components/ui/button'
+import { Select } from '@/components/ui/select'
 
 export const Route = createRootRoute({
   component: RootLayout,
@@ -9,33 +14,53 @@ export const Route = createRootRoute({
 
 export default function RootLayout() {
   const { i18n } = useTranslation()
+  const { toast } = useToast()
 
   return (
-    <div className="font-poppins">
-      <div className="flex justify-between gap-2 p-2">
-        <div className="flex items-center gap-2">
-          <Link to="/" className="[&.active]:font-bold">
-            Home
-          </Link>
-          <Link to="/about" className="[&.active]:font-bold">
-            About
-          </Link>
-        </div>
-        <div className="flex items-center gap-2">
-          {langs.map((lang) => (
-            <button
-              key={lang.value}
-              onClick={() => i18n.changeLanguage(lang.value)}
-              className="rounded p-2 transition-colors hover:bg-slate-100"
+    <div className="dark min-h-screen bg-background font-poppins text-foreground">
+      <ThemeProvider>
+        <div className="flex justify-between gap-2 p-2">
+          <div className="flex items-center gap-2">
+            <Button variant="outline" asChild>
+              <Link to="/" className="[&.active]:font-bold">
+                Home
+              </Link>
+            </Button>
+            <Button variant="outline" asChild>
+              <Link to="/about" className="[&.active]:font-bold">
+                About
+              </Link>
+            </Button>
+          </div>
+          <div className="flex items-center gap-2">
+            <Select.Root value={i18n.language} onValueChange={(ev) => i18n.changeLanguage(ev)}>
+              <Select.Trigger>
+                <Select.Value placeholder="Select option" />
+              </Select.Trigger>
+              <Select.Content>
+                {langs.map((lang) => (
+                  <Select.Item key={lang.value} value={lang.value}>
+                    {lang.label}
+                  </Select.Item>
+                ))}
+              </Select.Content>
+            </Select.Root>
+            <Button
+              onClick={() => {
+                toast({
+                  title: 'Uh oh! Something went wrong.',
+                  description: 'There was a problem with your request.',
+                })
+              }}
             >
-              {lang.label}
-            </button>
-          ))}
+              🍞 Toast
+            </Button>
+          </div>
         </div>
-      </div>
-      <hr />
-      <Outlet />
-      <TanStackRouterDevtools />
+        <Toaster />
+        <Outlet />
+        <TanStackRouterDevtools />
+      </ThemeProvider>
     </div>
   )
 }
